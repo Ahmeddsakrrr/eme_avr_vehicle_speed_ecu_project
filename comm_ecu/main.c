a4fd9f657f2c5c16438c645f562dceb11bf6fe2b
MennaTullah Ahmed <menna0969@gmail.com>
2023-09-11 11:22:08 +0200

    refactor (main): cleanup

--- comm_ecu/main.c
+++ comm_ecu/main.c
@@ -7,15 +7,9 @@
 
 
 
-#include "mem_map.h"
-#include "std.h"
-#include "spi_interface.h"
-#include "i2c_config.h"
-#include "eeprom.h"
-#include "app_private.h"
-#include "app_config.h"
-#include "app_interface.h"
 
+
+#include "app_interface.h"
 int main(void)
 {
      app_init();
