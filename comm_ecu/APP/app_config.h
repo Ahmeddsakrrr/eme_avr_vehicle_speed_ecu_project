4251672241a6479def62524d6b88cdfe30308109
MennaTullah Ahmed <menna0969@gmail.com>
2023-09-11 11:19:01 +0200

    update (app): define F_CPU

--- comm_ecu/APP/app_config.h
+++ comm_ecu/APP/app_config.h
@@ -9,6 +9,10 @@
 #ifndef APP_CONFIG_H_
 #define APP_CONFIG_H_
 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
 #define   APP_CAR_SPD_LIMIT_MIN_SPEED   30
 #define   APP_CAR_MAX_SPEED             220
 
